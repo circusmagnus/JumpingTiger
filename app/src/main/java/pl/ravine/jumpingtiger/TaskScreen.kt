2e86a829fb01c875ff1ac032f7e54680b5e718d9
circusmagnus <circusmagnus@gmail.com>
2023-05-08 15:24:10 +0200

    Show errored text

--- app/src/main/java/pl/ravine/jumpingtiger/TaskScreen.kt
+++ app/src/main/java/pl/ravine/jumpingtiger/TaskScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,10 +19,12 @@ import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
@@ -48,7 +51,8 @@ internal fun TaskScreen(state: TigerScreenState) {
                 json()
             }
         }
-        val question: Question = client.get("https://calculate-jvm-2-nhgp6wxvtq-lm.a.run.app/task?level=HARD").body()
+        val question: Question =
+            client.get("https://calculate-graal-mvn-nhgp6wxvtq-lm.a.run.app/").body()
         value = question
         awaitDispose {
             client.close()
@@ -63,6 +67,10 @@ internal fun TaskScreen(state: TigerScreenState) {
         mutableStateOf("")
     }
 
+    var isCorrect by remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(modifier = Modifier.padding(8.dp)) { paddingValues ->
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
@@ -71,18 +79,25 @@ internal fun TaskScreen(state: TigerScreenState) {
                 .fillMaxWidth()
                 .padding(paddingValues)
         ) {
-            Text(text = question.task)
+            Text(text = question.task, fontSize = 30.sp)
             TextField(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth(),
                 value = answer,
-                onValueChange = { typed -> answer = typed }
+                textStyle = LocalTextStyle.current.copy(color = if (isCorrect) Color.Black else Color.Red),
+                onValueChange = { typed ->
+                    answer = typed
+                    isCorrect = true
+                }
             )
             TextButton(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(Orange),
                 onClick = {
-                    if (answer.equals(question.result, ignoreCase = true)) state.hamburgerConsumed()
+                    isCorrect = answer.equals(question.result, ignoreCase = true)
+                    if (isCorrect) state.hamburgerConsumed()
+
                 }
             ) {
                 Text(textAlign = TextAlign.Center, text = stringResource(id = R.string.send))
